b82d7e3b54b727b6cb4200b7262ae9f179aa08e0
isidorosp <isidoros.passadis@gmail.com>
2021-02-11 13:29:46 +0100

    Update repo link
    From `/ceramic-indexer` to `/tiles`

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -37,7 +37,7 @@ const Footer = () => {
       </Text>
       <a href="https://mechanaut.xyz">
         <Flex alignItems="center">
-          <Link href="https://github.com/oaksprout/ceramic-document-viewer">
+          <Link href="https://github.com/oaksprout/tiles">
             <AiFillGithub size="24" />
           </Link>
         </Flex>
