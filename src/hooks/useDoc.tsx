cee2b2d5c914d54fe42c490545df7bb6181a549a
oaksprout <oaksproutthetan@gmail.com>
2021-02-11 05:29:26 +0000

    document list populates from firestore

--- src/hooks/useDoc.tsx
+++ src/hooks/useDoc.tsx
@@ -9,27 +9,3 @@ const getDocById = async (docId: string) => {
 export default function useDoc(docId: any) {
   return useQuery(['doc', docId], () => getDocById(docId))
 }
-
-// import { useState } from 'react'
-// import { ceramic } from '../App'
-
-// const useDoc = async (docId: any) => {
-//   const [docObj, setDocObj] = useState({
-//     isLoading: false,
-//     error: '',
-//     data: {},
-//   })
-
-//   setDocObj({ ...docObj, isLoading: true })
-
-//   await ceramic
-//     .loadDocument(docId)
-//     .then((res) => {
-//       setDocObj({ ...docObj, isLoading: false, data: res })
-//     })
-//     .catch((err) => setDocObj({ ...docObj, error: err }))
-
-//   return docObj
-// }
-
-// export default useDoc
