f8b443ba1c8f1b91ec81d0d04e083f6ce2b9db39
isidorosp <isidoros.passadis@gmail.com>
2021-02-11 12:21:00 +0100

    feat: add pagination

--- src/DocumentList.tsx
+++ src/DocumentList.tsx
@@ -11,45 +11,73 @@ import {
   Tr,
 } from '@chakra-ui/react'
 import { Link as ReachLink, RouteComponentProps } from '@reach/router'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useCollection } from 'react-firebase-hooks/firestore'
 import LoadingTableRows from './components/LoadingTableRows'
 import db from './firebase'
 import moment from 'moment'
 
-interface DocList {
-  docs: [{ docId: string }]
-}
-
 interface DocListProps extends RouteComponentProps {}
 
 const DocumentList = (props: DocListProps) => {
-  // const [paginateLimit, setpaginateLimit] = useState(15);
   const paginateLimit = 15
   const [paginatePage, setpaginatePage] = useState(0)
 
+  const [firstDoc, setFirstDoc] = useState<any | null>(null)
+  const [lastDoc, setLastDoc] = useState<any | null>(null)
+
+  const initQuery = db.collection('documents')
+    .orderBy('timestamp', 'desc')
+    .limit(paginateLimit)
+
+  const [firebaseQuery, setFirebaseQuery] = useState(initQuery)
+
   const [value, loading, error] = useCollection(
-    db.collection('documents').orderBy('timestamp', 'desc')
+    firebaseQuery
   )
 
-  // const dataTop = docData.docs.slice(
-  //   paginateLimit * paginatePage,
-  //   paginateLimit * paginatePage + paginateLimit
-  // )
+  useEffect(() => {
+  //   // Update the document title using the browser API
+    if (value && value.docs) {
+      console.log('updating firstdoc & lastdoc')
+      setFirstDoc(value.docs[0])
+      setLastDoc(value.docs[paginateLimit-1])
+      if (firstDoc !== null) {
+        console.log(firstDoc.data())
+        console.log(lastDoc.data())
+      }
+    }
+  }, [value]); // if we add what the warning suggests we get stuck in a recursive loop
 
   const handlePrev = () => {
-    if (paginatePage > 0) setpaginatePage(paginatePage - 1)
+    if (firstDoc != null) {
+      console.log(firstDoc.data())
+      const prevQuery = db.collection('documents').orderBy('timestamp', 'desc').endBefore(firstDoc.data().timestamp).limitToLast(paginateLimit)
+      setFirebaseQuery(prevQuery)
+      setpaginatePage(paginatePage-1)
+    }
   }
 
   const handleNext = () => {
-    setpaginatePage(paginatePage + 1)
+    if (lastDoc != null) {
+      console.log(lastDoc.data())
+      const nextQuery = db.collection('documents').orderBy('timestamp', 'desc').startAfter(lastDoc.data().timestamp).limit(paginateLimit)
+      setFirebaseQuery(nextQuery)
+      setpaginatePage(paginatePage+1)
+    }
   }
 
   if (error) return <strong>Error: {JSON.stringify(error)}</strong>
 
   return (
-    <Box px={{ base: 0, md: 6 }}>
-      {/* <HStack fontSize="sm" divider={<StackDivider />}>
+    <Flex
+      justifyContent="space-between"
+      flexDir="column"
+      alignItems="center"
+      px={6}
+      py={6}
+    >
+      <HStack fontSize="sm" divider={<StackDivider />}>
         <Text onClick={handlePrev}>
           <Link href="#">Prev</Link>
         </Text>
@@ -57,7 +85,7 @@ const DocumentList = (props: DocListProps) => {
         <Text onClick={handleNext}>
           <Link href="#">Next</Link>
         </Text>
-      </HStack> */}
+      </HStack>
       <Box width="100%">
         <Table>
           <Thead>
