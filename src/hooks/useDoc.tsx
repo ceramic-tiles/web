ab7781fca11d3f8f749952fce0d3e1e85c5a3daa
Oaksprout <66292936+oaksprout@users.noreply.github.com>
2021-04-03 16:51:24 +0100

    revert to any type (#18)
    added for now, we can clean up later

--- src/hooks/useDoc.tsx
+++ src/hooks/useDoc.tsx
@@ -2,7 +2,7 @@ import { useQuery } from 'react-query'
 import DocID from "@ceramicnetwork/docid";
 import { ceramic } from '../App'
 
-const getDocById = async (docId: DocID) => {
+const getDocById = async (docId: any) => {
   const doc = await ceramic.loadDocument(docId).then((res) => res)
   return doc
 }
