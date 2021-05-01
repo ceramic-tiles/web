2e29e251ba6dd2f07cbfbc6086baa799c29c1a6e
oaksprout <oaksproutthetan@gmail.com>
2021-05-01 18:37:40 +0100

    Misc visual tweaks

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,10 +5,9 @@ import { AiFillGithub } from 'react-icons/ai'
 const Footer = () => {
   return (
     <Flex
-      my={6}
       borderTopWidth={1}
-      px={6}
-      py={6}
+      px={2}
+      py={2}
       alignItems="center"
       justifyContent="space-between"
     >
